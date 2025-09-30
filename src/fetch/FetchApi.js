468812a1fc2400b15e146945151c46c930539c08
alemalagni <alemalagni@gmail.com>
2025-09-30 15:05:38 +0200

    add FetchApi module with getApi and getApiById functions for smartphone data retrieval

---
+++ src/fetch/FetchApi.js
@@ -0,0 +1,30 @@
+const urlAPI = 'http://localhost:3001/smartphones'
+
+export async function getApi() {
+    try {
+        const response = await fetch(urlAPI);
+
+        if (!response.ok) {
+            throw new Error('Errore nel server');
+        }
+        const data = await response.json();
+        return data;
+
+    } catch (error) {
+        console.error('L\'operazione non è andata a buon fine:', error);
+    }
+};
+
+export async function getApiById(id) {
+    try {
+        const response = await fetch(`${urlAPI}/${id}`);
+        if (!response.ok) {
+            throw new Error('Errore nel server');
+        }
+        const data = await response.json();
+        return data;
+
+    } catch (error) {
+        console.error('L\'operazione non è andata a buon fine:', error);
+    }
+};
