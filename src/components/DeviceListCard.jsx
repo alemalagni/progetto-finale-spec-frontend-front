b9208346caf80f6fe49bc223f86fbec102a4ce53
alemalagni <alemalagni@gmail.com>
2025-09-30 15:11:30 +0200

    add DeviceListCard component and integrate it into HomePage for device listing

---
+++ src/components/DeviceListCard.jsx
@@ -0,0 +1,7 @@
+export default function DeviceListCard({ key }) {
+    return (
+        <div>
+            <h2>Device List Card.jsx</h2>
+        </div>
+    );
+}
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,31 @@
+import { useState, useEffect } from "react";
+import { getApi } from "../fetch/FetchApi";
+import DeviceListCard from "../components/DeviceListCard";
+
 export default function HomePage() {
+    const [devices, setDevices] = useState([]);
+
+    useEffect(() => {
+        const fetchDevices = async () => {
+            try {
+                const data = await getApi();
+                setDevices(data || []);
+            } catch (error) {
+                console.error("Errore nel caricamento dei dispositivi:", error);
+                setDevices([]);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchDevices();
+    }, []);
+
     return (
         <div>
-            <h1>Home Page.jsx</h1>
+            {devices && devices.map(device => (
+                <DeviceListCard key={device.id} device={device} />
+            ))}
         </div>
     );
 }
