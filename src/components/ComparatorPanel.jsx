11250df19a7905393e25b2d551fdd65c6ad9828b
alemalagni <alemalagni@gmail.com>
2025-10-01 00:33:04 +0200

    refactor ComparatorPanel to fetch and display device data in a table format

--- src/components/ComparatorPanel.jsx
+++ src/components/ComparatorPanel.jsx
@@ -1,15 +1,55 @@
+import { useEffect, useState } from "react";
+import { getApiById } from "../fetch/FetchApi";
+
 export default function ComparatorPanel({ selectedDevices = [] }) {
+    const [devices, setDevices] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    const fetchDevices = async () => {
+        if (selectedDevices.length === 0) {
+            setDevices([]);
+            return;
+        } else {
+            const devicesData = await Promise.all(
+                selectedDevices.map(id => getApiById(id))
+            );
+
+            const extractedData = devicesData.map(response =>
+                response.smartphone
+            );
+            setDevices(extractedData);
+        }
+    }
+
+    useEffect(() => {
+        fetchDevices();
+        setLoading(false)
+    }, [selectedDevices]);
+
+    if (loading) {
+        return <div>Caricamento...</div>;
+    }
 
     return (
         <div>
             <h2>Comparator Panel</h2>
             {selectedDevices.length > 0 && (
-                <ul>
-                    {selectedDevices.map((id, index) => (
-                        <li key={index}>{id}</li>
-                    ))}
-                </ul>
-            )}
-        </div>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Prodotto</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {devices.map((device, index) => (
+                            <tr key={selectedDevices[index]}>
+                                <td>{device?.title}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            )
+            }
+        </div >
     );
 }
