e97aa46002caf783453c60a147e9a16d5a523a25
alemalagni <alemalagni@gmail.com>
2025-10-01 01:18:56 +0200

    style: refactor DeviceListCard to use Link and button for improved navigation

--- src/components/DeviceListCard.jsx
+++ src/components/DeviceListCard.jsx
@@ -1,10 +1,13 @@
+import { Link } from 'react-router-dom'
 import '../css/DeviceListCard.css'
 
 export default function DeviceListCard({ device }) {
     return (
-        <div className="device-list-card">
-            <h2 className="device-card-title">{device.title}</h2>
-            <p className="device-card-category">{device.category}</p>
-        </div>
+        <Link to={`/${device.id}`} className="device-card-link">
+            <button className="device-list-card">
+                <h2 className="device-card-title">{device.title}</h2>
+                <p className="device-card-category">{device.category}</p>
+            </button>
+        </Link>
     );
 }
