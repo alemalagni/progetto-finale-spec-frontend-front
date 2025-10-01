8e58df99d828814a85484e31c1268c9fdc4b1445
alemalagni <alemalagni@gmail.com>
2025-10-01 09:11:04 +0200

    style: add FavoritePage component and update routing in App for improved navigation

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes, BrowserRouter } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import DetailPage from './pages/DetailPage'
 import ComparatorPage from './pages/ComparatorPAge'
+import FavoritePage from './pages/FavoritePage'
 import NavBar from './components/NavBar'
 import './App.css'
 
@@ -15,6 +16,7 @@ export default function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/:id" element={<DetailPage />} />
           <Route path="/compare" element={<ComparatorPage />} />
+          <Route path="/favorites" element={<FavoritePage />} />
         </Routes>
       </BrowserRouter>
     </>
