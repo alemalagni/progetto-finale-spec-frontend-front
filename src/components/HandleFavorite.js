e79b97ec9fb58bb4be1cb351cab3b465b8a90a92
alemalagni <alemalagni@gmail.com>
2025-10-01 09:22:32 +0200

    style: implement favorite devices functionality in FavoritePage

---
+++ src/components/HandleFavorite.js
@@ -0,0 +1,18 @@
+const favoriteDevices = [1];
+
+export function addFavorites(id) {
+    if (!favoriteDevices.includes(id)) {
+        favoriteDevices.push(id);
+    }
+}
+
+export function removeFavorites(id) {
+    const index = favoriteDevices.findIndex(device => device.id === id);
+    if (index !== -1) {
+        favoriteDevices.splice(index, 1);
+    }
+}
+
+export function getFavoriteDevices() {
+    return favoriteDevices;
+}
--- src/pages/FavoritePage.jsx
+++ src/pages/FavoritePage.jsx
@@ -1,7 +1,45 @@
+import { getFavoriteDevices } from "../components/HandleFavorite";
+import { useEffect, useState } from "react";
+import { getApiById } from "../fetch/FetchApi";
+
 export default function FavoritePage() {
+    const favoriteDevices = getFavoriteDevices();
+    const [devices, setDevices] = useState([]);
+
+    const fetchDevices = async () => {
+        if (favoriteDevices.length === 0) {
+            setDevices([]);
+            return;
+        } else {
+            const devicesData = await Promise.all(
+                favoriteDevices.map(id => getApiById(id))
+            );
+
+            const extractedData = devicesData.map(response =>
+                response.smartphone
+            );
+            setDevices(extractedData);
+        }
+    }
+
+    useEffect(() => {
+        fetchDevices();
+    }, [favoriteDevices]);
+
     return (
         <div>
             <h1>Preferiti</h1>
+            <ul>
+                {favoriteDevices.length > 0 ? (
+                    devices.map((device, index) => (
+                        <li key={favoriteDevices[index]}>
+                            {device.title}
+                        </li>
+                    ))
+                ) : (
+                    <li>Nessun dispositivo preferito trovato.</li>
+                )}
+            </ul>
         </div>
     );
 }
